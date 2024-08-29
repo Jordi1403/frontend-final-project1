23be69e61c508cd91e34f19834127a3709a3f35a
jordi1403 <yarden.cohen.1403@gmail.com>
2024-08-29 04:33:46 -0700

    firebase config

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { AppComponent } from './app/app.component';
-import { environment } from '../environments/environment';
+import { environment } from './environments/environment'
 
 bootstrapApplication(AppComponent, {
   providers: [
