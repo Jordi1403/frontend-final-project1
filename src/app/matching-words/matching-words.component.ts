77b117772f71359b18ca2d79c65539aa75c5db84
daniel levitan <dani451451@gmail.com>
2024-09-03 13:29:00 +0300

    matching wors category fixed

--- src/app/matching-words/matching-words.component.ts
+++ src/app/matching-words/matching-words.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  OnDestroy,
+} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Category } from '../../shared/model/category';
+import { CategoriesService } from '../services/categories.service';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
+import { ExitConfirmationDialogComponent } from '../exit-confirmation-dialog/exit-confirmation-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-matching-words',
-  standalone: true,
-  imports: [],
   templateUrl: './matching-words.component.html',
-  styleUrl: './matching-words.component.css'
+  styleUrls: ['./matching-words.component.css'],
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MatchingWordsComponent {
+export class MatchingWordsComponent implements OnInit, OnDestroy {
+  currentCategory?: Category;
+  private routeSub?: Subscription;
 
+  constructor(
+    private categoriesService: CategoriesService,
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.routeSub = this.route.paramMap.subscribe((params) => {
+      const id = parseInt(params.get('id')!, 10);
+      if (!isNaN(id)) {
+        this.currentCategory = this.categoriesService.get(id);
+        if (!this.currentCategory) {
+          console.error('Category not found for ID:', id);
+        }
+      } else {
+        console.error('Invalid category ID:', params.get('id'));
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSub?.unsubscribe();
+  }
+
+  exitGame(): void {
+    this.dialog
+      .open(ExitConfirmationDialogComponent)
+      .afterClosed()
+      .subscribe((result) => {
+        if (result === 'yes') {
+          this.router.navigate(['/choose-game']);
+        }
+      });
+  }
 }
